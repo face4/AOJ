9315f0640cb82898995d5d7ac452d87d06f9395c
face4 <s1611361@u.tsukuba.ac.jp>
2019-10-04 11:56:07 +0900

    Update 1357.cpp

--- Volume13/1357.cpp
+++ Volume13/1357.cpp
@@ -7,15 +7,13 @@ int main(){
     int n;
     cin >> n;
     double d[n], r[n];
-    for(int i = 0; i < n; i++)  cin >> d[i];
-    r[0] = d[0];
-    double ans = r[0]+d[0];
-    for(int i = 1; i < n; i++){
-        double tmp = d[i];
+    for(int i = 0; i < n; i++)  cin >> r[i];
+    double ans = 0;
+    for(int i = 0; i < n; i++){
+        d[i] = r[i];
         for(int j = 0; j < i; j++){
-            tmp = max(tmp, r[j]+sqrt(pow(d[i]+d[j],2.0)-pow(fabs(d[i]-d[j]),2.0)));
+            d[i] = max(d[i], d[j]+2*sqrt(r[i]*r[j]));
         }
-        r[i] = tmp;
         ans = max(ans, r[i]+d[i]);
     }
     cout << fixed << setprecision(12) << ans << endl;
